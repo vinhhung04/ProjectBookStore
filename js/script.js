500b695c239a0cce4b1aab55af8fd03a70356333
vinhhung04 <165561928+vinhhung04@users.noreply.github.com>
2024-04-20 03:02:15 +0700

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -48,3 +48,85 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 
+
+                                                                            //Slide hinh
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+                                                                    //    video background
+  function resizeVideo() {
+    var video = document.getElementById('background-video');
+    var container = document.getElementById('background-video-container');
+    var aspectRatio = video.videoWidth / video.videoHeight;
+
+    var containerWidth = container.offsetWidth;
+    var containerHeight = container.offsetHeight;
+    var containerAspectRatio = containerWidth / containerHeight;
+
+    if (containerAspectRatio > aspectRatio) {
+        video.style.width = '100%';
+        video.style.height = 'auto';
+    } else {
+        video.style.width = 'auto';
+        video.style.height = '100%';
+    }
+}
+window.onload = function() {
+    resizeVideo();
+};
+
+window.onresize = function() {
+    resizeVideo();
+};
+
+                                                                            //Tat mo am thanh
+document.addEventListener('DOMContentLoaded', function () {
+    var video = document.getElementById('background-video');
+    var toggleSoundBtn = document.getElementById('toggle-sound-btn');
+
+    toggleSoundBtn.addEventListener('click', function () {
+        if (video.muted) {
+            video.muted = false; // Bật âm lượng
+            toggleSoundBtn.classList.remove('sound-off');
+            toggleSoundBtn.classList.add('sound-on');
+        } else {
+            video.muted = true; // Tắt âm lượng
+            toggleSoundBtn.classList.remove('sound-on');
+            toggleSoundBtn.classList.add('sound-off');
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+  const hoverables = document.querySelectorAll('.hoverable');
+  const firstSach = document.querySelector('.sach1');
+
+  hoverables.forEach(function (hoverable, index) {
+    const sach = document.querySelector(`.sach${index + 1}`);
+    hoverable.addEventListener('mouseover', function () {
+      firstSach.classList.remove('show');
+      sach.classList.add('show');
+    });
+
+    hoverable.addEventListener('mouseout', function () {
+      sach.classList.remove('show');
+      firstSach.classList.add('show');
+    });
+  });
+  
+  firstSach.classList.add('show');
+});
